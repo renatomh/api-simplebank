5438a85f1e7b11482d06c51ca89facd9f6fdeeea
Renato Marino Henz <renatomarinohenz@gmail.com>
2023-02-13 18:26:42 -0300

    chore: updated tests and transfer transaction

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -94,7 +94,22 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		// TODO: update accounts balance
+		// Updating accounts balance
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
