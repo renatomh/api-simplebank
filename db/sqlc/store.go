9a0163013fa20003993e72ec8de6ccb3e554438e
Renato Marino Henz <renatomarinohenz@gmail.com>
2023-02-13 19:16:04 -0300

    chore: avoid deadlock

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -94,25 +94,49 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		// Updating accounts balance
-		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
-		if err != nil {
-			return err
+		// Updating accounts balance, avoiding deadlocks
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(
+				ctx, q, arg.FromAccountID,
+				-arg.Amount, // money is moving out
+				arg.ToAccountID, arg.Amount,
+			)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(
+				ctx, q, arg.ToAccountID,
+				arg.Amount, // money is moving in
+				arg.FromAccountID, -arg.Amount,
+			)
 		}
 
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		return nil
+		return err
 	})
 
 	return result, err
+}
+
+// addMoney encapsulates the functions to add two account's balances, avoiding deadlocks
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	// Adding balance to the first account
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return // this is the same as: return account1, account2, err
+	}
+
+	// Adding balance to the second account
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	return
 }
